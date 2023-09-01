88453d5e972ecefd27e5e6cb7d6d012244b7743b
andreibromaskevis <126252917+andreibromaskevis@users.noreply.github.com>
2023-09-01 15:36:02 -0300

    Aula2 - ExGoto1.cpp

---
+++ Aula2 - ExGoto1.cpp
@@ -0,0 +1,26 @@
+//Crie um programa que exiba os números divisíveis por um até cem, utilizando a instrução 'goto'
+#include <stdio.h>
+#include <locale.h>
+
+int main() {
+
+setlocale(LC_ALL, "Portuguese");
+
+//Para alterar a divisão do loop, é necessário mudar os valores de (num) e (div), 
+    int num = 3;
+    int div = 3;
+
+loop:
+    if (num % div == 0) {
+    	printf("%d --> é divisível por ((%d))\n ", num, div);
+    }
+
+    num += 3;
+
+    if (num <= 100) {
+        goto loop;
+    }
+}	
+
+
+
