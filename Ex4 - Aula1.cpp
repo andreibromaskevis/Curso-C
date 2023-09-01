7a79c7993d999b5ef6b3d97a5ac93b555c1e7653
andreibromaskevis <126252917+andreibromaskevis@users.noreply.github.com>
2023-09-01 10:00:25 -0300

    Ex4 - Aula1.cpp

---
+++ Ex4 - Aula1.cpp
@@ -0,0 +1,20 @@
+//Atribua o valor 7 à variável “x”
+//Atribua o valor 3 à variável “y”
+//Atribua a multiplicação de “x” por “y” a variável “z”
+//Mostre o valor das variáveis “x”, “y” e “z” assim (3 x 7=21)
+
+#include <stdio.h>
+
+int main(){
+	
+	int x, y, z;
+	
+	x = 7;
+	y = 3;
+	z = x * y;
+	
+	printf("(%i x %i=%i)", x, y, z);
+	
+	
+}
+
