cde49bb3c4264b21b020c12eacc2453f21ea744b
andreibromaskevis <126252917+andreibromaskevis@users.noreply.github.com>
2023-09-05 15:59:25 -0300

    Aula4 - While2.cpp

---
+++ Aula4 - While2.cpp
@@ -0,0 +1,26 @@
+//Escreva um algoritmo que solicita ao usua´rio N notas, calcula e imprime na tela do computador a me´dia do aluno. 
+//O programa deve continuar solicitando notas ate´ que o usua´rio entre com -1. (Usando while)
+
+#include <stdio.h>
+#include <locale.h>
+
+int main() {
+	
+    setlocale(LC_ALL, "Portuguese");
+
+    float nota, soma, media, i;
+    
+    printf("Digite uma nota: ");
+    scanf("%f", &nota);
+    
+    while (nota != -1){
+    	
+    i++;
+    soma += nota;
+    media = soma / i;
+    
+    printf("Digite outra nota: ");
+    scanf("%f", &nota);
+	}
+    printf("Media das notas: %.2f", media);
+}
