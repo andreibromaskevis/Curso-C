fb62220504af26a18b6b6ee02fc28dfa3abb1ea9
andreibromaskevis <126252917+andreibromaskevis@users.noreply.github.com>
2023-09-15 11:57:28 -0300

    Aula1 - PE.cpp

---
+++ Aula1 - PE.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+    setlocale(LC_ALL,"Portuguese");
+
+    float real;
+    float dolar = 5.0;
+    float euro = 5.2;
+
+    printf("Digite um valor em real: R$");
+    scanf("%f", &real);
+
+    float real_dolar = real / dolar;
+    float real_euro = real / euro;
+
+    printf("Valor em dólar: %.2f\n", real_dolar);
+    printf("Valor em euro: %.2f\n", real_euro);
+
+    return 0;
+
+}
+
+
